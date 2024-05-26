ab335cb8afd03f30c76240cec0e309e8ffef2dcc
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-05-26 11:04:20 -0300

    En Leccion02 agregamos tipos de datos

---
+++ Javascript/Leccion02/02-01-TipoDatos.js
@@ -0,0 +1,18 @@
+// Comentarios de una sola linea
+/* 
+La sintaxis en los comentarios
+es igual a Java
+*/
+var nombre = "Juan"; //Tipo Str
+console.log(nombre);
+
+var num = 10; //Tipo numerico
+console.log(num);
+
+var object = {
+    nombre: "Juan",
+    apellido: "Orellana",
+    dni: 123456789,
+    telefono: 555-555-555
+}; //Tipo object
+console.log(object);
--- Javascript/Leccion02/02-01-TipoDatos}.js
+++
@@ -1,7 +0,0 @@
-// Comentarios de una sola linea
-/* 
-La sintaxis en los comentarios
-es igual a Java
-*/
-var nombre = "Juan";
-console.log(nombre);
