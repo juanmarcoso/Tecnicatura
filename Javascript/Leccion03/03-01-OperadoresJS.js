9fe8bc56198067fdfda1d59430a6d3805b98ab09
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-05-26 14:02:44 -0300

    Hemos creado dos ejercicios

---
+++ Javascript/Leccion03/03-01-OperadoresJS.js
@@ -0,0 +1,17 @@
+    //Ejercicio para encontrar los numeros pares e impares
+    let parImpar = 8;
+    if(parImpar % 2 == 0){
+        console.log("Es un numero PAR");
+    }
+    else{
+        console.log("Es un numero IMPAR");
+    }
+
+    // Ejercicio es mayor de edad o menor?
+    let edad = 23, adulto = 18;
+    if(edad >= adulto){
+        console.log("Usted es una persona adulta");
+    }
+    else{
+        console.log("Usted es menor de edad.");
+    }
