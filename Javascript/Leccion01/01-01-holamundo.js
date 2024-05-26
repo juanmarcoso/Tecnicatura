f300c5e9a0c3d347b91aa5458fc03036e487ff9c
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-05-26 10:13:40 -0300

    Haciendo los primeros ejercicios JS

---
+++ Javascript/Leccion01/01-01-holamundo.js
@@ -0,0 +1,5 @@
+// Variables tipo String e impresion por consola en la web
+var nombre = "Juan"; //Las variables en JS son de tipo dinamicas
+nombre = "Marcos";
+var apellido = "Orellana"
+console.log(nombre + " " + apellido);
