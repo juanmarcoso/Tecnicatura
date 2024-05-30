f8c5fccdc53cb2bca1626da5b63046d16a4fd091
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-05-30 08:32:22 -0300

    Ordenando archivos

--- Python/Ejercicio3.py
+++
@@ -1,4 +0,0 @@
-#Ejercicio 3
-#Preguntar la edad al usuario
-#Si la edad esta dentro de los 20 o 30 años, esta dentro del rango
-#Combinamos los operadores and y or para saber si el usuario esta dentro del rango o no
