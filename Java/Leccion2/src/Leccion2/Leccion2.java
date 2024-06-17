f3829ab963365c093d13ba193783f2e515be12e9
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-06-17 11:10:12 -0300

    Agregando leccion 2

---
+++ Java/Leccion2/src/Leccion2/Leccion2.java
@@ -0,0 +1,13 @@
+package Leccion2;
+
+public class Leccion2 {
+    public static void main(String[] args) {
+        var condicion = false;
+        if (condicion) {
+            System.out.println("Condicion Verdadera"); //Condicional simple
+        }
+        else {
+            System.out.println("Condicion Falsa"); //Condicional dooble
+        }
+    }
+}
